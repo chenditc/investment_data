6f842adb29e10d6155886b659dfe0d62136ca398
chendi <chenditc@umich.edu>
2022-08-09 11:19:54 +0000

    Update qlib dump script

--- qlib/dump_all_to_qlib_source.py
+++ qlib/dump_all_to_qlib_source.py
@@ -16,7 +16,7 @@ def dump_all_to_sqlib_source(skip_exists=True):
     print("Dumping to file: ", filename)
     if skip_exists and os.path.isfile(filename):
         continue
-    stock_df = pd.read_sql(f"select * from final_a_stock_eod_price where symbol='{symbol}'", dbConnection)
+    stock_df = pd.read_sql(f"select *, amount/volume*10 as vwap from final_a_stock_eod_price where symbol='{symbol}'", dbConnection)
     stock_df.to_csv(filename, index=False)
 
 if __name__ == "__main__":
