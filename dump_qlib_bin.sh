9fe3049740c880b68d1230cdc66ad6ffcde20804
Di <chenditc@umich.edu>
2023-06-25 21:57:15 +0800

    Update dump_qlib_bin.sh

--- dump_qlib_bin.sh
+++ dump_qlib_bin.sh
@@ -6,13 +6,11 @@ dolt pull origin
 dolt sql-server &
 mkdir ./qlib/qlib_source
 python3 ./qlib/dump_all_to_qlib_source.py
-killall dolt
 
 export PYTHONPATH=$PYTHONPATH:/qlib/scripts
 python3 ./qlib/normalize.py normalize_data --source_dir ./qlib/qlib_source/ --normalize_dir ./qlib_normalize --max_workers=16 --date_field_name="tradedate" 
 python3 /qlib/scripts/dump_bin.py dump_all --csv_path ./qlib_normalize/ --qlib_dir ./qlib_bin --date_field_name=tradedate --exclude_fields=tradedate,symbol
 
-dolt sql-server &
 mkdir ./qlib/qlib_index/
 python3 ./qlib/dump_index_weight.py 
 python3 ./tushare/dump_day_calendar.py ./qlib_bin/
