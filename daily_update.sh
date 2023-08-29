2f673381df4030b3cfef31a3b1a44556eedec18c
Di <chenditc@umich.edu>
2023-08-29 19:46:50 +0800

    Update daily_update.sh

--- daily_update.sh
+++ daily_update.sh
@@ -1,29 +1,30 @@
 set -e
 set -x
 
-dolt init || dolt remote add origin chenditc/investment_data || dolt pull origin
+[ ! -d "/dolt/investment_data" ] && echo "initializing dolt repo" && cd /dolt && dolt clone chenditc/investment_data
+cd /dolt
 
 echo "Updating index weight"
 startdate=$(dolt sql -q "select * from max_index_date" -r csv | tail -1)
-python3 tushare/dump_index_weight.py --start_date=$startdate
-for file in $(ls tushare/index_weight/); 
+python3 /investment_data/tushare/dump_index_weight.py --start_date=$startdate
+for file in $(ls /investment_data/tushare/index_weight/); 
 do  
-  dolt table import -u ts_index_weight tushare/index_weight/$file; 
+  dolt table import -u ts_index_weight /investment_data/tushare/index_weight/$file; 
 done
 
 echo "Updating index price"
-python3 tushare/dump_index_eod_price.py 
-for file in $(ls tushare/index/); 
+python3 /investment_data/tushare/dump_index_eod_price.py 
+for file in $(ls /investment_data/tushare/index/); 
 do   
-  dolt table import -u ts_a_stock_eod_price tushare/index/$file; 
+  dolt table import -u ts_a_stock_eod_price /investment_data/tushare/index/$file; 
 done
 
 echo "Updating stock price"
 dolt sql-server &
-python3 tushare/update_a_stock_eod_price_to_latest.py
+sleep 5 && python3 /investment_data/tushare/update_a_stock_eod_price_to_latest.py
 killall dolt
 
-dolt sql --file ./tushare/regular_update.sql
+dolt sql --file /investment_data/tushare/regular_update.sql
 
 dolt add -A
 
