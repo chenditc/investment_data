3c8cd9edf4ac980959fdbea72571181817cb9473
Di <chenditc@umich.edu>
2025-10-28 18:45:16 +0800

    Update daily_update.sh

--- daily_update.sh
+++ daily_update.sh
@@ -3,8 +3,8 @@ set -x
 
 [ ! -d "/dolt/investment_data" ] && echo "initializing dolt repo" && cd /dolt && dolt clone chenditc/investment_data
 cd /dolt/investment_data
-dolt pull
-dolt push
+dolt pull origin master
+dolt push origin master
 
 echo "Updating index weight"
 startdate=$(dolt sql -q "select * from max_index_date" -r csv | tail -1)
@@ -39,7 +39,7 @@ else
     echo "Changes found. Committing and pushing..."
     # Run the necessary commands
     dolt commit -m "Daily update"
-    dolt push 
+    dolt push origin master
     echo "Changes committed and pushed."
 fi
 
